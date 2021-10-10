e009e600fa9ba832c765fa3a90fb1f680768ab1e
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-10-10 07:35:12 +0700

    share viewmodel between fragments

--- app/src/main/java/com/example/android/lifecycles/step5/Fragment_step5.java
+++ app/src/main/java/com/example/android/lifecycles/step5/Fragment_step5.java
@@ -18,13 +18,20 @@ package com.example.android.lifecycles.step5;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 
 import com.example.android.codelabs.lifecycle.R;
+import com.example.android.lifecycles.step2.ChronometerViewModel;
 
 /**
  * Shows a SeekBar that should be synced with a value in a ViewModel.
@@ -43,6 +50,8 @@ public class Fragment_step5 extends Fragment {
         mSeekBar = root.findViewById(R.id.seekBar);
 
         // TODO: get ViewModel
+        mSeekBarViewModel
+                = new ViewModelProvider(this).get(SeekBarViewModel.class);
         subscribeSeekBar();
 
         return root;
@@ -56,6 +65,8 @@ public class Fragment_step5 extends Fragment {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // TODO: Set the ViewModel's value when the change comes from the user.
+                Log.d("Step5", "Progress changed!");
+                mSeekBarViewModel.seekbarValue.setValue(progress);
             }
 
             @Override
@@ -66,6 +77,13 @@ public class Fragment_step5 extends Fragment {
         });
 
         // TODO: Update the SeekBar when the ViewModel is changed.
-        // mSeekBarViewModel.seekbarValue.observe(...
+        mSeekBarViewModel.seekbarValue.observe( requireActivity(), new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer value) {
+                if (value != null) {
+                    mSeekBar.setProgress(value);
+                }
+            }
+        });
     }
 }
